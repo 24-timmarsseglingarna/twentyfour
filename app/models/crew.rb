9348022dc8df828071d7f363762759794192dd8b
Stefan Pettersson <stefan.pettersson@lumano.se>
2014-11-03 10:03:25 +0100

    Starting with a participant list.

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -2,7 +2,7 @@ class Crew < ActiveRecord::Base
   
   belongs_to  :boat
   belongs_to  :race
-  belongs_to  :regattum #only to be able to change races within the same regattum
+  belongs_to  :regattum # consider remove
   belongs_to  :point # start_point. TODO Consider attribute name change
   has_many    :members
   has_many    :persons, :through => :members
@@ -10,6 +10,21 @@ class Crew < ActiveRecord::Base
   
   validates   :race, presence: true 
 
+  def goal
+    if race.regattum.common_goal_only
+      race.regattum.common_goal
+    else
+      if race.regattum.common_goal.present?
+        if common_goal
+          race.regattum.common_goal
+        else
+          point
+        end
+      else
+        point
+      end
+    end
+  end
 
   def name
     id.to_s
